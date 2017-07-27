263857c459941a2e9b7bd0727257b74c490a6128
Isaac Peifer <iwpeifer@gmail.com>
2017-07-27 11:43:30 -0400

    hooked up to backend, can now retrieve data from Yelp

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      businessPool: []
+    }
+  }
+
+  componentWillMount() {
+    return fetch(`http://localhost:3000/retrieve?term=${'chinese'}&location=${'new jersey'}`)
+    .then( res => res.json() )
+    .then( json => {
+      if(json.error) {
+        alert("Could not execute search, try altering the location and/or search term.")
+      } else {
+        console.log(json)
+        // this.setState({
+        //   yelp: json,
+        //   locationInput: '',
+        //   termInput: ''
+        // })
+      }
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      TEST
       </div>
     );
   }
