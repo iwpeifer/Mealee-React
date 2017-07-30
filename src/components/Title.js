83b07e68a5b98a86c9167a76fa0cea71ef9e31e0
Isaac Peifer <iwpeifer@gmail.com>
2017-07-30 11:18:29 -0400

    switched title image a bit apparently

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+
+  return (
+    <div className='title-bar'>
+      <div className='yelp-logo'></div>
+      <div className='title'>Mealée!</div>
+    </div>
+  )
+}
